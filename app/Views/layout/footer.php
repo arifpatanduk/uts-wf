105cb08278729f2911381d645aa7ba23c761e6a9
arifpatanduk <arifpatanduk2@gmail.com>
2021-04-15 00:12:08 +0700

    styling & layouting

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,62 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h3>PTIK</h3>
+                    <p>Kampus V JPTK FKIP UNS Pabelan <br>
+                        Jl. Jend. Ahmad Yani 200A <br>
+                        Pabelan, Kartasura,<br>
+                        Sukoharjo 57100 <br><br>
+                        <strong>Phone:</strong> (0271)648939<br>
+                        <strong>Email:</strong> ptik@fkip.uns.ac.id<br>
+                    </p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Sekolah Mitra</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://smkn1sukoharjo.sch.id/">SMK N 1 Sukoharjo</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://smkn2solo.sch.id/">SMK N 2 Surakarta</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://www.smkn3sukoharjo.sch.id/">SMK N 3 Surakarta</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="http://www.smkn5solo.sch.id">SMK N 5 Surakarta</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://smkn6solo.sch.id/">SMK N 6 Surakarta</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://www.smkn2kra.sch.id/">SMK N 2 Karanganyar</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="http://web.smkn1sawitboyolali.sch.id/">SMK N 1 Sawit Boyolali</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="http://www.smkn1banyudono.sch.id/">SMK N 1 Banyudono</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Internal Links</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://ptik.fkip.uns.ac.id/skripsi/">Sistem Informasi Pemantauan Tugas Akhir (SIPTA)</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://spada.uns.ac.id/">SPADA UNS</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://siakad.uns.ac.id/">SIAKAD UNS</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://ocw.uns.ac.id/">OCW UNS</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="https://ptik.fkip.uns.ac.id/piptik/">Sistem Informasi Praktik Industri PTIK UNS</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Our Social Networks</h4>
+                    <div class="social-links mt-3">
+                        <a href="https://twitter.com/PTIK_uns" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="https://www.youtube.com/channel/UCy6CiXSV1WvH514Qxy7mXeQ" class="youtube"><i class='bx bxl-youtube'></i></a>
+                        <a href="https://www.instagram.com/ptik_uns/" class="instagram"><i class="bx bxl-instagram"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container py-4">
+        <div class="copyright">
+            &copy; Copyright <strong><span>PTIK</span></strong>. All Rights Reserved
+        </div>
+    </div>
+</footer><!-- End Footer -->
---
+++ app/Views/layout/header.php
@@ -0,0 +1,36 @@
+<!-- ======= Header ======= -->
+<header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+        <a href="/" class="logo"><img src="/img/logo.png" alt=""></a>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+
+                <li class="dropdown"><a href="#about"><span>Profil</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Profil Prodi PTIK</a></li>
+                        <li><a href="#">Visi, Misi, dan Tujuan</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown"><a href="#"><span>Akademik</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Kurikulum</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown"><a href="#"><span>Kemahasiswaan</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Prestasi Mahasiswa PTIK</a></li>
+                        <li><a href="#">Himpunan Mahasiswa</a></li>
+                    </ul>
+                </li>
+                <li><a class="nav-link scrollto" href="#">Portofolio</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
